b324d7e06a5e5bffa3e574e395e1e85e761cc354
Rajitha10 <tejorajitha@gmail.com>
2018-04-15 16:14:02 +0530

    First AreaShapes Folder

---
+++ AreaShapes/src/Rectangle.java
@@ -0,0 +1,17 @@
+
+public class Rectangle 
+{
+	int length=5;
+	int breadth=2;
+	int a;
+	
+	
+	public void area()
+	{
+		a=(int) (0.5*length*breadth);
+		System.out.println(a);
+		
+	}
+	
+
+}
---
+++ AreaShapes/src/RectangleTest.java
@@ -0,0 +1,12 @@
+
+public class RectangleTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Rectangle r=new Rectangle();
+		r.area();
+		
+
+	}
+
+}
